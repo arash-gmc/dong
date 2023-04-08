5ceebedbad194fb1eb87422fe7d6bd63c96f83c3
arash-gmc <arash.green.423@gmail.com>
2023-04-08 09:48:44 +0330

    load data bug fixed

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -25,8 +25,18 @@ class Main extends Component {
 
     loadData = ()=>{
         const data = JSON.parse(localStorage.getItem('data'))
-        if(data)
-            return data
+        const peoplesKeys = ['id','name','motherPay']
+        const payKeys = ['id','ownerId','name','amount','equal','show','paidFor','unequalPays']
+        if (!data)
+            return
+        if (data.peoples[0])
+            if (peoplesKeys.some(pk=>data.peoples[0][pk]===undefined))
+                return
+        if (data.pays[0])
+            if (payKeys.some(pk=>data.pays[0][pk]===undefined))
+                return
+        
+        return data    
     }
 
     resetAll = ()=>{
